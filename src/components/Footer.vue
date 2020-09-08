1e50dcc2c1035d98c76134d580bfbf429a0de524
joaomoschetta <joaopedromschneider123@gmail.com>
2020-09-08 05:48:14 -0300

    Footer and navbar visual

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    
-  </div>
+  <footer>
+    <p>
+      <a href="https://github.com/joaomoschetta">Developer GitHub</a>
+       - 
+      <a href="https://github.com/joaomoschetta/newtab-extra-utilities">Project GitHub</a>
+       - 
+      2020
+    </p>
+  </footer>
 </template>
+
+<style lang="scss">
+  @import "@/scss/_variables";
+
+  footer {
+    background: $background-light-color;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 50px;
+
+    p, a {
+      display: inline;
+      text-decoration: none;
+      color: white;
+    }
+  }
+</style>
--- src/components/PageNavBar.vue
+++ src/components/PageNavBar.vue
@@ -1,5 +1,59 @@
 <template>
-  <div>
-    
-  </div>
+  <nav>
+    <ul>
+      <li alt="Home navigation button">
+        <img src="@/assets/home-icon.png" alt="House icon">
+      </li>
+      <div class="separate-bar"></div>
+      <li alt="Spreasheet navigation button">
+        <img src="@/assets/sheet-icon.svg" alt="Spreadsheet icon">
+      </li>
+      <div class="separate-bar"></div>
+      <li alt="Help navigation button">
+        <img src="@/assets/interrogation-icon.png" alt="Interrogation icon">
+      </li>
+      <div class="separate-bar"></div>
+      <li alt="Settings navigation button">
+        <img src="@/assets/config-icon.png" alt="Gear icon">
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<style lang="scss">
+  @import "@/scss/_variables";
+
+  nav {
+    background: $background-dark-color;
+
+    ul {
+      list-style: none;
+      padding: 30px 15px 15px;
+
+      li {
+        height: 85px;
+        width: 85px;
+        padding: 15px;
+        margin: 15px auto 15px;
+        border-radius: 50%;
+        background-color: $primary-color;
+
+          &:hover {
+            background: $primary-dark-color;
+          }
+
+          img {
+            max-width: 100%;
+            align-self: center;
+            justify-self: center;
+          }
+      }
+
+      .separate-bar {
+        width: 100%;
+        height: 1px;
+        background: $background-light-color;
+      }
+    }
+  }
+</style>
