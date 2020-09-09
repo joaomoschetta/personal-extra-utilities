8f4cc473b400a28f2adf48a0b6bbe1c4aabaa44a
joaomoschetta <joaopedromschneider123@gmail.com>
2020-09-09 05:35:07 -0300

    Progressive and regressive timer visual

--- src/components/home/MainContent.vue
+++ src/components/home/MainContent.vue
@@ -1,13 +1,77 @@
 <template>
   <main>
-    
+    <div class="timers">
+      <ProgressiveTimer class="prog" />
+      <RegressiveTimer class="regr" />
+      <PomodoroTimer class="pomo" />
+    </div>
+
+    <div class="note-pads">
+
+    </div>
+
+    <div class="utils">
+
+    </div>
   </main>
 </template>
+
+<script>
+import ProgressiveTimer from './Timers/Progressive';
+import RegressiveTimer from './Timers/Regressive';
+import PomodoroTimer from './Timers/Pomodoro';
+
+export default {
+    components: {
+      ProgressiveTimer,
+      RegressiveTimer,
+      PomodoroTimer,
+    },
+}
+</script>
 
 <style lang="scss">
   @import "@/scss/_variables";
 
   main {
     background: $background-color;
+    padding: 2.5rem 8rem;
+
+    display: grid;
+    grid-template-areas: 
+      "timer timer timer"
+      "note note utils";
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1fr 4fr;
+    gap: 30px;
+
+    .timers {
+      grid-area: timer;
+
+      display: flex;
+      justify-content: space-between;
+      align-items: stretch;
+
+      & > * {
+        flex-basis: calc(100% - 60px);
+        margin-right: 30px;
+
+        border-radius: 15px;
+        padding: 1rem;  
+        background: yellow;
+
+        &:nth-child(3) {
+          margin-right: 0;
+        }
+      }
+    }
+
+    .note-pads {
+      grid-area: note;
+    }
+
+    .utils {
+      grid-area: utils;
+    }
   }
 </style>
---
+++ src/components/home/Timers/Progressive.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="timer">
+    <div class="left">
+      <p>Progressive Timer</p>
+      <p class="time-display">00:00</p>
+    </div>
+
+    <div class="right">
+      <img src="@/assets/play-icon.png" alt="Play icon">
+      <img src="@/assets/pause-icon.png" alt="Pause icon">
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import "@/scss/_timerStyle.scss";
+</style>
