75cf7d7677abfa5051e3338e91e4b4343e3ead7b
joaomoschetta <joaopedromschneider123@gmail.com>
2020-09-14 00:15:17 -0300

    ScrollBar style move from notes to your own scss

--- src/components/home/Notes.vue
+++ src/components/home/Notes.vue
@@ -3,6 +3,7 @@
 </template>
 
 <style scoped lang="scss">
+
   textarea {
     width: 100%;
     height: 100%;
@@ -11,24 +12,12 @@
 
     resize: none;
 
+    // scroll bar style
+    @import "@/scss/_scrollBar.scss";
+
     // hide selected border
     &:focus, input:focus{
       outline: 0;
-    }
-
-    // scroll bar style
-    &::-webkit-scrollbar {
-      width: 10px;
-      height: 6px;
-    }
-
-    &::-webkit-scrollbar-thumb {
-      background: #2b6c42;
-      border: 0px none #ffffff;
-    }
-    &::-webkit-scrollbar-track {
-      background: #9a9a9a;
-      border: 21px none #ffffff;
     }
   }
 </style>
