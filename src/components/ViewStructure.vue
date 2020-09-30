6774409f91400d4a75602c1cf6cc4e3d8f2c3dbd
joaomoschetta <joaopedromschneider123@gmail.com>
2020-09-30 16:27:55 -0300

    Main content setted by routes

--- src/components/ViewStructure.vue
+++ src/components/ViewStructure.vue
@@ -2,7 +2,8 @@
   <div class="structure">
     <div class="main-grid">
       <PageNavBar class="side-nav-bar"/>
-      <MainContent class="main-content"/>
+      <HomeMainContent v-if="homePage()" class="main-content"/>
+      <SpreadsheetMainContent v-if="sheetPage()" class="main-content" />
     </div>
 
     <Footer class=".footer"/>
@@ -12,14 +13,32 @@
 <script>
 import PageNavBar from './PageNavBar';
 import Footer from './Footer';
-import MainContent from './home/MainContent';
+import HomeMainContent from './home/MainContent';
+import SpreadsheetMainContent from './spreadsheets/MainContent';
 
 export default {
   components: {
     PageNavBar,
     Footer,
-    MainContent
-  }
+    HomeMainContent,
+    SpreadsheetMainContent,
+  },
+  methods: {
+    homePage: function() {
+      if(this.$route.path == "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    sheetPage: function() {
+      if(this.$route.path == "/spreadsheet") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
 }
 </script>
 
