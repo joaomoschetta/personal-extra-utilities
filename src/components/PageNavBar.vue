688e66f0c3490a56a2b955f255c50ce07f4120c3
joaomoschetta <joaopedromschneider123@gmail.com>
2020-10-02 14:54:39 -0300

    Nav bar background depending current route add

--- src/components/PageNavBar.vue
+++ src/components/PageNavBar.vue
@@ -2,27 +2,44 @@
   <nav>
     <ul>
       <router-link to="/">
-        <li alt="Home navigation button">
+        <li :class="liRouteClass('/')" alt="Home navigation button" >
           <img src="@/assets/home-icon.png" alt="House icon">
         </li>
       </router-link>
       <div class="separate-bar"></div>
+
       <router-link to="/spreadsheet">
-        <li alt="Spreasheet navigation button">
+        <li :class="liRouteClass('/spreadsheet')" alt="Spreasheet navigation button">
           <img src="@/assets/sheet-icon.svg" alt="Spreadsheet icon">
         </li>
       </router-link>
       <div class="separate-bar"></div>
-      <li alt="Help navigation button">
+
+      <li :class="liRouteClass('/help')" alt="Help navigation button">
         <img src="@/assets/interrogation-icon.png" alt="Interrogation icon">
       </li>
       <div class="separate-bar"></div>
-      <li alt="Settings navigation button">
+      
+      <li :class="liRouteClass('/settings')" alt="Settings navigation button">
         <img src="@/assets/config-icon.png" alt="Gear icon">
       </li>
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    liRouteClass: function(route) {
+      if (this.$route.path == route) {
+        return 'current-route';
+      } else {
+        return 'common';
+      }
+    }
+  },
+}
+</script>
 
 <style lang="scss">
   @import "@/scss/_variables";
@@ -40,17 +57,14 @@
         padding: 15px;
         margin: 15px auto 15px;
         border-radius: 50%;
-        background-color: $primary-color;
 
-          &:hover {
-            background: $primary-dark-color;
-          }
+        
 
-          img {
-            max-width: 100%;
-            align-self: center;
-            justify-self: center;
-          }
+        img {
+          max-width: 100%;
+          align-self: center;
+          justify-self: center;
+        }
       }
 
       .separate-bar {
@@ -58,6 +72,24 @@
         height: 1px;
         background: $background-light-color;
       }
+    }
+  }
+
+  //classes to define background
+  //  example: if you are in the Home page the Home listItem receive current-route class
+  .common {
+    background-color: $primary-color;
+
+    &:hover {
+      background: $primary-dark-color;
+    }
+  }
+
+  .current-route {
+    background: $secondary-color;
+
+    &:hover {
+      background: $secondary-dark-color;
     }
   }
 </style>
